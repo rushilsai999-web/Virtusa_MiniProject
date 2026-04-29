e0828563111d968be40ad835c3fad4d7bbebf922
rushilsai999-web <rushilsai999@gmail.com>
2026-04-30 02:43:38 +0530

    PasswordValidator.java

--- PasswordValiadator/PasswordValidator.java
+++ PasswordValiadator/PasswordValidator.java
@@ -6,23 +6,19 @@ public class PasswordValidator {
 		PasswordChecker checker = new PasswordChecker();
 		int tries = 0;
 		final int MAX = 5;
-
 		while (tries < MAX) {
 			System.out.print("Enter password: ");
 			String pwd = scanner.nextLine();
 			System.out.println("You entered: " + PasswordUtils.obfuscatePassword(pwd));
-
 			if (checker.isCompliant(pwd)) {
 				int s = checker.score(pwd);
 				System.out.println("Password accepted.");
 				System.out.println("Strength: " + PasswordUtils.strengthCategory(s));
 				break;
 			}
-
 			tries++;
 			System.out.println("Remaining attempts: " + (MAX - tries));
 		}
-
 		if (tries == MAX) System.out.println("No attempts left. Exiting.");
 		scanner.close();
 	}
