9aa5079dddac824c28b4c55f059fe659ef0b7bed
rushilsai999-web <rushilsai999@gmail.com>
2026-04-30 02:42:50 +0530

    PasswordChecker.java

--- PasswordValiadator/PasswordChecker.java
+++ PasswordValiadator/PasswordChecker.java
@@ -6,12 +6,10 @@ public class PasswordChecker implements PasswordPolicy {
 			System.out.println("Password too short (minimum 8 characters)");
 			return false;
 		}
-
 		boolean hasUpper = false;
 		boolean hasLower = false;
 		boolean hasDigit = false;
 		boolean hasSymbol = false;
-
 		for (int i = 0; i < length; i++) {
 			char c = password.charAt(i);
 			if (Character.isUpperCase(c)) hasUpper = true;
@@ -19,7 +17,6 @@ public class PasswordChecker implements PasswordPolicy {
 			else if (Character.isDigit(c)) hasDigit = true;
 			else hasSymbol = true;
 		}
-
 		if (!hasUpper) System.out.println("Please include at least one uppercase letter");
 		if (!hasLower) System.out.println("Please include at least one lowercase letter");
 		if (!hasDigit) System.out.println("Please include at least one digit");
@@ -27,15 +24,12 @@ public class PasswordChecker implements PasswordPolicy {
 
 		return hasUpper && hasLower && hasDigit && hasSymbol;
 	}
-
 	public int score(String password) {
 		if (password == null) return 0;
 		int points = 0;
 		int length = password.length();
-
 		if (length >= 8) points += 1;
 		if (length >= 12) points += 1;
-
 		boolean hasUpper = false, hasLower = false, hasDigit = false, hasSymbol = false;
 		for (char ch : password.toCharArray()) {
 			if (Character.isUpperCase(ch)) hasUpper = true;
@@ -43,12 +37,10 @@ public class PasswordChecker implements PasswordPolicy {
 			else if (Character.isDigit(ch)) hasDigit = true;
 			else hasSymbol = true;
 		}
-
 		if (hasUpper) points += 1;
 		if (hasLower) points += 1;
 		if (hasDigit) points += 1;
 		if (hasSymbol) points += 1;
-
 		return points;
 	}
 }
