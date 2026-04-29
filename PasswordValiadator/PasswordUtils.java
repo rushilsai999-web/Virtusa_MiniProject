1d881e552fa6443f7f0b4ad3c3f2793dc6fd236e
rushilsai999-web <rushilsai999@gmail.com>
2026-04-30 02:46:21 +0530

    PasswordUtils.java

--- PasswordValiadator/PasswordUtils.java
+++ PasswordValiadator/PasswordUtils.java
@@ -1,14 +1,18 @@
 public class PasswordUtils {
 	public static String obfuscatePassword(String password) {
-		if (password == null) return "";
+		if (password == null) 
+			return "";
 		StringBuilder sb = new StringBuilder(password.length());
-		for (int i = 0; i < password.length(); i++) sb.append('*');
+		for (int i = 0; i < password.length(); i++) 
+			sb.append('*');
 		return sb.toString();
 	}
 
 	public static String strengthCategory(int score) {
-		if (score <= 2) return "Weak";
-		if (score <= 4) return "Moderate";
+		if (score <= 2) 
+			return "Weak";
+		if (score <= 4) 
+			return "Moderate";
 		return "Strong";
 	}
 }
